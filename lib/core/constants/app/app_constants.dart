7f2f7da72abff8a6c9302521c2d6deb3ab5a9639
Özgür ERDOĞAN <erdgn54@gmail.com>
2022-11-08 22:40:50 +0300

    context extension and email extension added

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -2,4 +2,5 @@ class ApplicationConstants {
   static const LANG_ASSET_PATH = "asset/lang";
   static const IPAD_NAME = "IPAD";
   static const COMPANY_NAME = 'Ozgur';
+  static const EMAIL_REGEX = r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]\b' ;
 }
